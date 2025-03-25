aead5d8194818ae5c0328262a2a372dde16f5d0f
Rs4anti <44727222+Rs4anti@users.noreply.github.com>
2025-03-25 22:01:07 +0100

    sistemato mockup db

--- provaDjango/djangotutorial/chatLLM/views.py
+++ provaDjango/djangotutorial/chatLLM/views.py
@@ -2,14 +2,14 @@ from django.http import HttpResponse, JsonResponse
 from langchain_community.llms import Ollama
 
 def index(request):
-    return HttpResponse("Hello, world! Questa l'homepage per chattare con un LLM.")
+    return HttpResponse("Hello, world! Questa è la pagina per chattare con un LLM.")
 
 
 # Inizializza Ollama con un modello locale
 llm = Ollama(model="mistral")
 
 def chat_response(request):
-    """Risponde a una query usando Ollama in locale con LangChain"""
+    #Risponde a una query usando Ollama in locale con LangChain
 
     #http://127.0.0.1:8000/chatLLM/query/?query=Chi%20ha%20scoperto%20l%27America%3F
     user_query = request.GET.get("query", "")
@@ -17,7 +17,7 @@ def chat_response(request):
     if not user_query:
         return JsonResponse({"error": "Nessuna query fornita"}, status=400)
 
-    # Genera la risposta con LangChain
+    # Risposta con LangChain
     response = llm.invoke(user_query)
 
     return JsonResponse({"response": response})
