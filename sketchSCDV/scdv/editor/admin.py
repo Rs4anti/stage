ad16d15fbfa71a1422b5ef19df8362b1ca93d3f8
Rs4anti <44727222+Rs4anti@users.noreply.github.com>
2025-04-14 12:27:06 +0200

    Ok task per atomic service con inserimento - sistemare codice

--- sketchSCDV/scdv/editor/admin.py
+++ sketchSCDV/scdv/editor/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import BPMNDiagram
 
-# Register your models here.
+admin.site.register(BPMNDiagram)
---
+++ sketchSCDV/scdv/editor/static/js/custom-moddle.js
@@ -0,0 +1,32 @@
+// custom-moddle.js
+const customModdle = {
+    name: 'custom',
+    uri: 'http://example.com/custom',
+    prefix: 'custom',
+    xml: {
+      tagAlias: 'lowerCase'
+    },
+    types: [
+      {
+        name: 'AtomicExtension',
+        superClass: ['Element'],
+        properties: [
+          {
+            name: 'atomicType',
+            isAttr: false,
+            type: 'String'
+          },
+          {
+            name: 'inputParams',
+            isAttr: false,
+            type: 'String'
+          },
+          {
+            name: 'outputParams',
+            isAttr: false,
+            type: 'String'
+          }
+        ]
+      }
+    ]
+  };  
