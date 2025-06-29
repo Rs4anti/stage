143b7171d48efe525c84ff100b876d9cdf4d2298
Rs4anti <44727222+Rs4anti@users.noreply.github.com>
2025-06-29 16:17:58 +0200

    rimozione api composite service

--- sketchSCDV/scdv/editor/urls.py
+++ sketchSCDV/scdv/editor/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.data_view_editor, name='editor'),
     path('api/save-diagram/', views.save_diagram, name='save_diagram'),
     path('api/save-atomic-service/', views.save_atomic_service, name='save_atomic_service'),
-    #path('api/save-composite-service/', views.save_composite_service, name='save_composite_service'),
     path('api/save-cpps-service/', views.save_cpps_service, name='save_cpps_service'),
     path('api/save-cppn-service/', views.save_cppn_service, name='save_cppn_service'),
 ]
