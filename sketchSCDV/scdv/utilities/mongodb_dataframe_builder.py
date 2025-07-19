ce6f5a60dc3fa28db5054d03cb2daf2946fce50d
Rs4anti <44727222+Rs4anti@users.noreply.github.com>
2025-07-19 16:36:25 +0200

    sistemato doc as

--- sketchSCDV/scdv/utilities/mongodb_dataframe_builder.py
+++ sketchSCDV/scdv/utilities/mongodb_dataframe_builder.py
@@ -16,22 +16,47 @@ class AtomicServiceDataFrameBuilder:
         }
         overview_df = pd.DataFrame(overview_data)
 
+        # Function to detect type
+        def detect_type(value):
+            if isinstance(value, bool):
+                return 'bool'
+            if isinstance(value, int):
+                return 'int'
+            if isinstance(value, float):
+                return 'float'
+            if isinstance(value, str):
+                val = value.strip()
+                if val.lower() in ['true', 'false']:
+                    return 'bool'
+                try:
+                    int(val)
+                    return 'int'
+                except ValueError:
+                    pass
+                try:
+                    float(val)
+                    return 'float'
+                except ValueError:
+                    pass
+                return 'string'
+            return 'unknown'
+
         # Parameters dataframe
         params_rows = []
 
         for param in doc.get('input_params', []):
             params_rows.append({
                 'task_id': doc['task_id'],
-                'param_name': param['value'],
-                'param_type': param['type'],
+                'param_name': param,
+                'param_type': detect_type(param),
                 'io_type': 'input'
             })
 
         for param in doc.get('output_params', []):
             params_rows.append({
                 'task_id': doc['task_id'],
-                'param_name': param['value'],
-                'param_type': param['type'],
+                'param_name': param,
+                'param_type': detect_type(param),
                 'io_type': 'output'
             })
 
